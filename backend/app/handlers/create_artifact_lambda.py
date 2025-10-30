4642bf2b8bec456af3c6b95d244a4ee49b022efe
aabougendia <22-101194@students.eui.edu.eg>
2025-10-29 23:15:51 -0400

    test first CD with AWS lambda

--- backend/app/handlers/create_artifact_lambda.py
+++ backend/app/handlers/create_artifact_lambda.py
@@ -1,22 +1,14 @@
 import json
 
-
 def lambda_handler(event, context):
-    # Parse the incoming event to extract artifact details
-    artifact_data = json.loads(event['body'])
-
-    # Here you would typically add code to store the artifact in a database
-    # For demonstration, we'll just return the received data
-
-    response = {
-        "statusCode": 201,
+    print(json.dumps(event))  # logs to CloudWatch
+    return {
+        "statusCode": 200,
+        "headers": {"Content-Type": "application/json"},
         "body": json.dumps({
-            "message": "Artifact created successfully",
-            "artifact": artifact_data
-        }),
-        "headers": {
-            "Content-Type": "application/json"
-        }
+            "message": f"{event['resource']} called successfully from Github actions",
+            "method": event['httpMethod'],
+            "pathParameters": event.get('pathParameters'),
+            "query": event.get('queryStringParameters'),
+        })
     }
-
-    return response
